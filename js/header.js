04c4631d6ca9c007458f2be539b7988537a2a592
zch0301 <291053953@qq.com>
2017-03-10 18:39:25 +0800

    导航栏效果

---
+++ js/header.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017/3/10 0010.
+ */
+require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js"],function (common,datecontrol,searchcontrol) {
+    var dateController = document.getElementById('hciDateBtn'),
+        searchController = document.getElementById('hciSearchBtn'),
+        nav = document.getElementById('hciNav');
+
+    common.addEvent(dateController,'click',datecontrol.datecontrol);
+    common.addEvent(searchController,'click',searchcontrol.searchcontrol);
+
+    window.onscroll = function () {
+        if(common.getScrollTop()>0){
+            common.addClass(nav,'scroll');
+        }else{
+            common.removeClass(nav,'scroll');
+        }
+    };
+})
