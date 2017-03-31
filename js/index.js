32654554ace47e36e42677a3d129e81f3a12ade4
zchwhu <291053953@qq.com>
2017-04-01 00:53:51 +0800

    删除首页轮播图，增加新闻版块

--- js/index.js
+++ js/index.js
@@ -1,17 +1,10 @@
 /**
  * Created by Administrator on 2017/2/15.
  */
-require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js","js/util/carousel.js"], function (common,datecontrol,searchcontrol,carousel) {
+require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js"], function (common,datecontrol,searchcontrol) {
     var dateController = document.getElementById('hciDateBtn'),
         searchController = document.getElementById('hciSearchBtn'),
         nav = document.getElementById('hciNav');
-
-    var hciCarouselContainer = document.getElementById('hciCarousel'),
-        hciCarouselInner = document.getElementById('hciCarouselInner');
-
-    var hciCarousel = carousel();
-
-    hciCarousel.init(hciCarouselContainer,hciCarouselInner);
 
     common.addEvent(dateController,'click',datecontrol.datecontrol);
     common.addEvent(searchController,'click',searchcontrol.searchcontrol);
