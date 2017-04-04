98d5565ab9af368f304da3d51a5675af11ef48d0
zchwhu <291053953@qq.com>
2017-04-05 01:27:43 +0800

    增加首页新闻版块

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 /**
  * Created by Administrator on 2017/2/15.
  */
-require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js"], function (common,datecontrol,searchcontrol) {
+require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js","js/util/newsbox.js"], function (common,datecontrol,searchcontrol,newsbox) {
     var dateController = document.getElementById('hciDateBtn'),
         searchController = document.getElementById('hciSearchBtn'),
         nav = document.getElementById('hciNav');
+
+    var container = document.querySelector('.hci-news-jumbotron-inner');
+    var list = document.querySelectorAll('.hci-news-item');
+
+    var hciNewsBox = newsbox(container,list);
 
     common.addEvent(dateController,'click',datecontrol.datecontrol);
     common.addEvent(searchController,'click',searchcontrol.searchcontrol);
