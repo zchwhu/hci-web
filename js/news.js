72e59e0e18d1a0844ce56aba793d418b6f899c6a
zch0301 <291053953@qq.com>
2017-04-08 20:43:20 +0800

    增加新闻动态页

---
+++ js/news.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2017/4/5 0005.
+ */
+require(["js/util/common.js","js/util/datecontrol.js","js/util/searchcontrol.js","js/util/pagination.js"], function (common,datecontrol,searchcontrol,pagination) {
+    var dateController = document.getElementById('hciDateBtn'),
+        searchController = document.getElementById('hciSearchBtn'),
+        nav = document.getElementById('hciNav');
+
+    var MAXSCROLLTOP = 548;
+    common.addEvent(dateController,'click',datecontrol.datecontrol);
+    common.addEvent(searchController,'click',searchcontrol.searchcontrol);
+
+    var prevBtn = document.getElementById('prev'),
+        nextBtn = document.getElementById('next'),
+        container = document.getElementById('hciPagination'),
+        pages = document.querySelectorAll('.page');
+
+    var hciPagination = pagination();
+    hciPagination.init(container,prevBtn,nextBtn,pages);
+
+    window.onscroll = function () {
+        if(common.getScrollTop()>MAXSCROLLTOP){
+            common.addClass(nav,'scroll');
+        }else{
+            common.removeClass(nav,'scroll');
+        }
+    };
+
+})
